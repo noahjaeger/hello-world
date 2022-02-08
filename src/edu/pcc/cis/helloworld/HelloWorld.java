b85d89480217e93b6fe5f251a51fc71bc6a2c12f
Noah Jaeger <nosajaeger@gmail.com>
2022-02-07 21:41:20 -0800

    added my name as an author

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,6 +2,10 @@ package edu.pcc.cis.helloworld;
 
 import java.util.Date;
 
+ /*
+ * @author Noah Jaeger
+ */
+
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
